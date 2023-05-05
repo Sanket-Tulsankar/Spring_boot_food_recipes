a5c804529c5c38e883e7830ff5e96183537f7b5a
Sanket Tulsankar <sankettulsankar009@gmail.com>
2023-05-05 14:39:35 +0530

    Connect to frontend

--- src/main/java/com/recipes/controller/SignUpController.java
+++ src/main/java/com/recipes/controller/SignUpController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import com.recipes.exceptionHandler.UserNotFoundException;
 import com.recipes.services.SignUpService;
 
 @RestController
+@CrossOrigin
 public class SignUpController {
 
 	@Autowired
